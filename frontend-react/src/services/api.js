1206712e5253bca1cc18febf6282ebbd120bee72
MonicaGunasekaran <137418588+MonicaGunasekaran@users.noreply.github.com>
2025-08-14 10:22:06 +0530

    Update api.js

--- frontend-react/src/services/api.js
+++ frontend-react/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const BASE = import.meta.env.VITE_API_URL || 'http://localhost:3000/api'
+const BASE = import.meta.env.VITE_API_URL || 'https://backendformernapp.onrender.com/'
 
 export const api = axios.create({
   baseURL: BASE,
