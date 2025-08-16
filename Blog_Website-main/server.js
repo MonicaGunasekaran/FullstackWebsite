9e61f5fce7b99626ada103dcdd1eecc240e4b1ff
MonicaGunasekaran <gmonica2593@gmail.com>
2025-08-16 23:21:55 +0530

    worked on cors

--- Blog_Website-main/server.js
+++ Blog_Website-main/server.js
@@ -7,16 +7,7 @@ const path = require("path");
 const app = express();
 
 // === Middleware ===
-app.use(
-  cors({
-    origin: [
-      "http://localhost:5173",
-      "https://funny-seahorse-3732de.netlify.app",
-    ],
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    credentials: true,
-  })
-);
+app.use(cors({ origin: "*", methods: ["GET", "POST", "PUT", "DELETE"] }));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
